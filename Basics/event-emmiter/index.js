a0cb945cbfecfe81ac864d148d15965f32d6392a
voliinyk-blackbird <voliinyk@blackbird-lab.com>
2024-11-18 13:27:44 +0100

    delete basics

--- Basics/event-emmiter/index.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events');
-
-const myEmitter = new EventEmitter();
-
-myEmitter.on('greet', (name) => {
-    console.log(`Hello ${name}`);
-})
-
-myEmitter.emit('greet', 'John');
