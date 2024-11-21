07a6f77ef2b4c0db624088bdf0f875a60240b1c7
voliinyk-blackbird <voliinyk@blackbird-lab.com>
2024-11-22 00:56:35 +0100

    port

--- SOCKETS/server.js
+++ SOCKETS/server.js
@@ -59,10 +59,6 @@ io.on('connection', (socket) => {
     })
 })
 
-
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(3000, () => {
+    console.log(`Server is running on port ${3000}`);
 })
